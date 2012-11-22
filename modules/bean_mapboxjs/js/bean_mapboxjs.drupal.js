4c50bffee34ab05f138822763697923a5ecac233
Sean Larkin <sean@thinkshout.com>
2012-11-22 12:43:27 -0800

    Adding first pass at Bean integration for MapBox.JS module.

---
+++ modules/bean_mapboxjs/js/bean_mapboxjs.drupal.js
@@ -0,0 +1,22 @@
+(function ($) {
+
+  Drupal.behaviors.bean_mapboxjs = {
+    attach: function (context, settings) {
+
+      $(settings.bean_mapboxjs).each(function () {
+
+        // Load a settings object with all of our map settings.
+        var settings = {};
+        for (var setting in this.configuration) {
+          settings[setting] = this.configuration[setting];
+        }
+
+        // Load a simple map example.
+        mapbox.auto(this.mapID, settings.tileset1);
+
+      });
+
+    }
+  }
+
+})(jQuery);
