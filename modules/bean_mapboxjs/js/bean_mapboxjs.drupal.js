294945877afa8f838cacb9f2e5e5acec17a5aca1
Sean Larkin <sean@thinkshout.com>
2012-11-28 23:39:23 -0800

    w00T! Got a tile switcher working.

--- modules/bean_mapboxjs/js/bean_mapboxjs.drupal.js
+++ modules/bean_mapboxjs/js/bean_mapboxjs.drupal.js
@@ -11,7 +11,7 @@
 					settings[setting] = this.configuration[setting];
 				}
 
-				// Load a map with the right ID and add some controls.
+				// Load a map with the right ID and optionally add some controls.
 				var map = mapbox.map(this.mapID);
 				if (settings.zoomer == 1) {
 					map.ui.zoomer.add();
@@ -20,12 +20,40 @@
 					map.ui.fullscreen.add();
 				}
 
+				// If the map switcher is included, grab it for later.
+				var options = document.getElementById('map-ui');
+
 				// Start adding our layers.
 				for (var i = 0; i < settings.tilesets.length; i++) {
-					layer = mapbox.layer().url(settings.tilesets[i]['url']);
-					map.addLayer(layer);
+					addLayer(i);
 				}
+
+				// Helper function for adding layers to map.
+				function addLayer(num) {
+					layer = mapbox.layer().url(settings.tilesets[num]['url'], function(data) {
+						// If "toggleable layers enabled, show in a layer switcher."
+						if (settings.layer_toggle == 1) {
+							var item = document.createElement('li');
+							var option = document.createElement('a');
+								option.href = '#';
+								option.id = data.name;
+								option.className = 'active';
+								option.innerHTML = settings.tilesets[num]['title'];
+							option.onclick = function(e) {
+								e.preventDefault();
+								e.stopPropagation();
+								map.getLayer(this.id).enabled ? map.getLayer(this.id).disable() : map.getLayer(this.id).enable();
+								this.className = map.getLayer(this.id).enabled ? 'active' : '';
+							};
+							item.appendChild(option);
+							options.appendChild(item);
+						}
+					});
+					map.addLayer(layer);
+				}(i);
+
 				// Center on the last loaded layer.
+				// @TODO - Centering and zooming are acting wonky.
 				mapbox.load(layer.url(), function(data) {
 					map.center({
 						lat: data.center.lat,
@@ -33,29 +61,6 @@
 					});
 					map.zoom(data.zoom, true);
 				});
-				// If "toggleable layers enabled, show in a layer switcher."
-				if (settings.layer_toggle == 1) {
-					var layers = document.getElementById('map-ui');
-					for (var i = 0; i < map.getLayers().length; i ++) {
-						var n = map.getLayerAt(i).name;
-						console.log(n);
-						var item = document.createElement('li');
-						var layer = document.createElement('a');
-							layer.href = '#';
-							layer.id = n;
-							layer.className = 'active';
-							layer.innerHTML = settings.tilesets[i]['title'];
-
-						layer.onclick = function(e) {
-							e.preventDefault();
-							e.stopPropagation();
-							map.getLayer(this.id).enabled ? map.getLayer(this.id).disable() : map.getLayer(this.id).enable();
-							this.className = map.getLayer(this.id).enabled ? 'active' : '';
-						};
-						item.appendChild(layer);
-						layers.appendChild(item);
-					}
-				}
 			})
 		}
 	}
