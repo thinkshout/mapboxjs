d605833be7e6cd6b9ec442a7289e84b4d54bc155
Sean Larkin <sean@thinkshout.com>
2012-12-22 15:14:28 -0600

    Adding support for MapBox tile legends.

--- js/mapboxjs.drupal.js
+++ js/mapboxjs.drupal.js
@@ -19,6 +19,9 @@
 				if (settings.fullscreen == 1) {
 					map.ui.fullscreen.add();
 				}
+				if (settings.legend == 1) {
+					map.ui.legend.add();
+				}
 				map.centerzoom({ lat: settings.lat, lon: settings.lon }, settings.zoom);
 
 				// If the map switcher ui element is included, grab it for later.
