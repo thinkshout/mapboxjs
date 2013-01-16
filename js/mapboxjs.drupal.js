878a56217b5914e786d95e7bea29c99036f1967f
Lev Tsypin <lev@thinkshout.com>
2013-01-16 10:58:36 -0800

    Major JS refactor to use a closure allowing for multiple maps on a page.

--- js/mapboxjs.drupal.js
+++ js/mapboxjs.drupal.js
@@ -3,47 +3,49 @@
   Drupal.behaviors.mapboxjs = {
     attach:function (context, settings) {
 
-      $(settings.mapboxjs).each(function () {
-        var mapObj = this;
+      for (var mapid in settings.mapboxjs) {
+        var mapObj = settings.mapboxjs[mapid];
+        var map = mapbox.map(mapid);
 
-        // Load a map with the right ID and optionally add some controls and variables.
-        var map = mapbox.map(mapObj.mapID);
-        if (mapObj.configuration.zoomer === 1) {
-          map.ui.zoomer.add();
-        }
-        if (mapObj.configuration.fullscreen === 1) {
-          map.ui.fullscreen.add();
-        }
-        if (mapObj.configuration.legend === 1) {
-          map.ui.legend.add();
-        }
-        map.centerzoom({ lat:mapObj.configuration.center.lat, lon:mapObj.configuration.center.lon }, mapObj.configuration.zoom);
+        // Note the closure needed here to ensure the right "map" is passed.
+        !function (mapObj, map) {
 
-        var base_layers = [];
-        for (var i in mapObj.layers.base) {
-          base_layers[i] = mapObj.layers.base[i].url;
-        }
+          if (mapObj.configuration.zoomer === 1) {
+            map.ui.zoomer.add();
+          }
+          if (mapObj.configuration.fullscreen === 1) {
+            map.ui.fullscreen.add();
+          }
+          if (mapObj.configuration.legend === 1) {
+            map.ui.legend.add();
+          }
+          map.centerzoom({ lat:mapObj.configuration.center.lat, lon:mapObj.configuration.center.lon }, mapObj.configuration.zoom);
 
-        mapbox.load(base_layers, function(data) {
-          Drupal.mapboxjs.load_layers(data, mapObj.layers.base, map, true);
-
-          // Add optional layers. Need to ensure this is done after base layers load.
-          var optional_layers = [];
-          for (var i in mapObj.layers.optional) {
-            optional_layers[i] = mapObj.layers.optional[i].url;
+          var base_layers = [];
+          for (var i in mapObj.layers.base) {
+            base_layers[i] = mapObj.layers.base[i].url;
           }
 
-          mapbox.load(optional_layers, function(data) {
-            Drupal.mapboxjs.load_layers(data, mapObj.layers.optional, map, false);
+          mapbox.load(base_layers, function(data) {
+            Drupal.mapboxjs.load_layers(data, mapObj.layers.base, map, true);
+
+            // Add optional layers. Need to ensure this is done after base layers load.
+            var optional_layers = [];
+            for (var i in mapObj.layers.optional) {
+              optional_layers[i] = mapObj.layers.optional[i].url;
+            }
+
+            mapbox.load(optional_layers, function(data) {
+              Drupal.mapboxjs.load_layers(data, mapObj.layers.optional, map, false);
+            });
+
           });
 
-        });
-
-        if (mapObj.configuration.interactive === 1) {
-          map.interaction.auto();
-        }
-
-      });
+          if (mapObj.configuration.interactive === 1) {
+            map.interaction.auto();
+          }
+        }(mapObj, map);
+      }
     }
   };
 
@@ -64,15 +66,14 @@
     load_layers: function (data, layers, map, base) {
       if (layers.length > 1 || !base) {
         var switcher = document.createElement('ul');
-            switcher.id = 'mapboxjs-switcher-' + (base ? 'base' : 'optional');
-            switcher.className = 'mapboxjs-switcher';
+            switcher.className = 'mapboxjs-switcher mapboxjs-switcher-' + (base ? 'base' : 'optional');
         for (var i = 0; i < data.length; i++) {
           var o = data[i];
           map.addLayer(o.layer);
           var item = document.createElement('li');
           var layer = document.createElement('a');
               layer.href = '#';
-              layer.id = o.layer.id();
+              layer.layer_id = o.layer.id();
               layer.className = 'mapboxjs-switcher-link';
               layer.innerHTML = layers[i].label;
 
@@ -89,19 +90,19 @@
             e.stopPropagation();
             var link = this;
             if (base) {
-              $('#mapboxjs-switcher-base a', $('#' + map.parent.id)).each(function(){
-                if (link.id === this.id) {
-                  map.getLayer(this.id).enable();
+              $('a', $(switcher)).each(function(){
+                if (link.layer_id === this.layer_id) {
+                  map.getLayer(this.layer_id).enable();
                   $(this).addClass('mapboxjs-layer-active');
                 }
                 else{
-                  map.getLayer(this.id).disable();
+                  map.getLayer(this.layer_id).disable();
                   $(this).removeClass('mapboxjs-layer-active');
                 }
               });
             }
             else {
-              map.getLayer(this.id).enabled ? map.getLayer(this.id).disable() : map.getLayer(this.id).enable();
+              map.getLayer(this.layer_id).enabled ? map.getLayer(this.layer_id).disable() : map.getLayer(this.layer_id).enable();
               $(this).toggleClass('mapboxjs-layer-active');
             }
 
@@ -116,10 +117,9 @@
       else {
         map.addLayer(data[0].layer);
       }
-      
+
       map.refresh();
     }
-
   };
 
 })(jQuery);
