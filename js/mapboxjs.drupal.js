cef400dcca0ee53ea5e2febe8c976d2cbc348396
Tauno Hogue <tauno@thinkshout.com>
2013-09-28 20:36:11 -0700

    Respect legend setting

--- js/mapboxjs.drupal.js
+++ js/mapboxjs.drupal.js
@@ -11,6 +11,18 @@
           settings['zoomControl'] = false;
         }
 
+        if (mapObj.configuration.scrollWheelZoom !== 1) {
+          settings['scrollWheelZoom'] = false;
+        }
+
+        if (mapObj.configuration.doubleClickZoom !== 1) {
+          settings['doubleClickZoom'] = false;
+        }
+
+        if (mapObj.configuration.dragging !== 1) {
+          settings['dragging'] = false;
+        }
+
         // @TODO more leaflet settings.
 
         var map = L.mapbox.map(mapid, null, settings);
@@ -37,59 +49,72 @@
      */
     render_map: function(mapObj, map) {
 
+      var interactive = false;
+      if (mapObj.configuration.interactive === 1) {
+        interactive = true;
+      }
+
       // Legend
       var legend = false;
       if (mapObj.configuration.legend === 1) {
         map.addControl(L.mapbox.legendControl());
         legend = true;
+
+        // Set triggers to add/remove legend contents and gridlayers based on active layer.
+        map.on('layeradd', function(e) {
+          if (e.layer.getTileJSON) {
+            var tileJSON = e.layer.getTileJSON();
+            if (legend) {
+              if (tileJSON && tileJSON.legend) {
+                map.legendControl.addLegend(tileJSON.legend);
+              }
+            }
+          }
+        });
+
+        map.on('layerremove', function(e) {
+          if (e.layer.getTileJSON) {
+            var tileJSON = e.layer.getTileJSON();
+            if (legend) {
+              if (tileJSON && tileJSON.legend) {
+                map.legendControl.removeLegend(tileJSON.legend);
+              }
+            }
+          }
+        });
       }
-
-      // Set triggers to add/remove legend contents and gridlayers based on active layer.
-      map.on('layeradd', function(e) {
-        if (e.layer.getTileJSON) {
-          var tileJSON = e.layer.getTileJSON();
-          if (legend) {
-            if (tileJSON && tileJSON.legend) {
-              map.legendControl.addLegend(tileJSON.legend);
-            }
-          }
-        }
-      });
-
-      map.on('layerremove', function(e) {
-        if (e.layer.getTileJSON) {
-          var tileJSON = e.layer.getTileJSON();
-          if (legend) {
-            if (tileJSON && tileJSON.legend) {
-              map.legendControl.removeLegend(tileJSON.legend);
-            }
-          }
-        }
-      });
 
       map.setView({ lat:mapObj.configuration.center.lat, lon:mapObj.configuration.center.lon }, mapObj.configuration.zoom);
 
       var base_layers = {};
       for (var i in mapObj.layers['base']) {
 
-        var layer = L.mapbox.tileLayer(mapObj.layers['base'][i]['url']);
-        var gridLayer = L.mapbox.gridLayer(mapObj.layers['base'][i]['url']);
+        var tile_layer = L.mapbox.tileLayer(mapObj.layers['base'][i]['url']);
 
-        if (mapObj.layers['base'][i]['active'] === true) {
-          // Async loading, so when ready check if we need to load the legend.
-          // Somehow doesn't trigger layeradd event.
-          layer.on('ready', function() {
-            // get TileJSON data from the loaded layer
-            var tileJSON = this.getTileJSON();
-            if (tileJSON && tileJSON.legend) {
-              map.legendControl.addLegend(tileJSON.legend);
-            }
-          });
-          map.addLayer(layer);
-
-          map.addLayer(gridLayer);
+        if (interactive) {
+          var gridLayer = L.mapbox.gridLayer(mapObj.layers['base'][i]['url']);
+          layer = L.layerGroup([tile_layer, gridLayer]);
           map.addControl(L.mapbox.gridControl(gridLayer));
         }
+        else {
+          layer = tile_layer;
+        }
+
+        if (mapObj.layers['base'][i]['active'] === true) {
+          if (legend) {
+            // Async loading, so when ready check if we need to load the legend.
+            tile_layer.on('ready', function() {
+              // get TileJSON data from the loaded layer
+              var tileJSON = this.getTileJSON();
+              if (tileJSON && tileJSON.legend) {
+                map.legendControl.addLegend(tileJSON.legend);
+              }
+            });
+          }
+
+          layer.addTo(map);
+        }
+
         base_layers[mapObj.layers['base'][i]['label']] = layer;
       }
 
